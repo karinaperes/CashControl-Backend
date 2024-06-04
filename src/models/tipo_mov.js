b7651ebc9ca48d788e331e74836cd22e459fc770
karinaperes <karinaperesg@gmail.com>
2024-06-04 16:28:25 -0300

    Inclusão modelos classe e tipomov

--- src/models/tipo_mov.js
+++ src/models/tipo_mov.js
@@ -1,9 +1,11 @@
 const { DataTypes } = require("sequelize") // Importando o componente DataTypes do Sequelize
 const { connection } = require("../database/connection")
+
 const TipoMov = connection.define('tipo_mov', {
 	tipo_mov: {
 		type: DataTypes.STRING,
         allowNull: false
 	}
 })
+
 module.exports = TipoMov
