28df9bb0242d1219c1870ba66d3f6467a23e2bb7
karinaperes <karinaperesg@gmail.com>
2024-06-18 16:48:40 -0300

    Inclusão arquivo migration movimentos, arquivo classe routes

---
+++ src/controllers/ClasseController.js
@@ -0,0 +1,11 @@
+const Classe = require('../models/Classe')
+
+// class ClasseController {
+//     async cadastrar(req, res) {
+//         try {
+
+//            // const classe = req. // TODO de onde vamos trazer essa informação?
+
+//         }
+//     }
+// }
