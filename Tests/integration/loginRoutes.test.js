f94068fc1641bfd4ed01cacbde6cadfa5122b1d4
Karina <karinaperesg@gmail.com>
2025-03-10 21:36:47 -0300

    Testes login controller

--- Tests/integration/loginRoutes.test.js
+++ Tests/integration/loginRoutes.test.js
@@ -1,14 +1,14 @@
-const request = require('supertest');
-const { Server } = require('../../src/server');
-const { connection } = require('../../src/database/connection');
-const Usuario = require('../../src/models/Usuario');
+const request = require("supertest");
+const { Server } = require("../../src/server");
+const { connection } = require("../../src/database/connection");
+const Usuario = require("../../src/models/Usuario");
 
 const serverInstance = new Server();
 
-describe('Testes de Integração - LoginController', () => {
+describe("Testes de Integração - LoginController", () => {
   let app;
   let usuarioCriado;
-  let senhaUsuario = 'senha123';
+  let senhaUsuario = "senha123";
 
   beforeAll(() => {
     app = serverInstance.getApp();
@@ -16,10 +16,10 @@ describe('Testes de Integração - LoginController', () => {
 
   beforeEach(async () => {
     await connection.sync({ force: true });
-    
+
     usuarioCriado = await Usuario.create({
-      nome: 'Usuário Teste',
-      email: 'teste@email.com',
+      nome: "Usuário Teste",
+      email: "teste@email.com",
       senha: senhaUsuario, // Assumindo que a senha já foi armazenada com hash
     });
   });
@@ -28,61 +28,68 @@ describe('Testes de Integração - LoginController', () => {
     await connection.close();
   });
 
-  describe('POST /login', () => {
-    it('deve autenticar um usuário com credenciais válidas e retornar um token', async () => {
-      const response = await request(app).post('/login').send({
-        email: 'teste@email.com',
+  describe("POST /login", () => {
+    it("deve autenticar um usuário com credenciais válidas e retornar um token", async () => {
+      const response = await request(app).post("/login").send({
+        email: "teste@email.com",
         senha: senhaUsuario,
       });
 
       expect(response.status).toBe(200);
-      expect(response.body).toHaveProperty('Token');
+      expect(response.body).toHaveProperty("Token");
     });
 
-    it('deve retornar erro ao tentar autenticar com senha incorreta', async () => {
-      const response = await request(app).post('/login').send({
-        email: 'teste@email.com',
-        senha: 'senhaErrada',
+    it("deve retornar erro ao tentar autenticar com senha incorreta", async () => {
+      const response = await request(app).post("/login").send({
+        email: "teste@email.com",
+        senha: "senhaErrada",
       });
 
       expect(response.status).toBe(400);
-      expect(response.body).toHaveProperty('mensagem', 'Senha inválida');
+      expect(response.body).toHaveProperty("mensagem", "Senha inválida");
     });
 
-    it('deve retornar erro ao tentar autenticar com email inexistente', async () => {
-      const response = await request(app).post('/login').send({
-        email: 'naoexiste@email.com',
-        senha: 'senha123',
+    it("deve retornar erro ao tentar autenticar com email inexistente", async () => {
+      const response = await request(app).post("/login").send({
+        email: "naoexiste@email.com",
+        senha: "senha123",
       });
 
       expect(response.status).toBe(404);
-      expect(response.body).toHaveProperty('erro', 'Email não corresponde a nenhum usuário');
+      expect(response.body).toHaveProperty(
+        "erro",
+        "Email não corresponde a nenhum usuário"
+      );
     });
 
-    it('deve retornar erro se o email estiver vazio ou apenas com espaços', async () => {
-      const response = await request(app).post('/login').send({
-        email: '  ',
-        senha: 'senha123',
+    it("deve retornar erro se o email estiver vazio ou apenas com espaços", async () => {
+      const response = await request(app).post("/login").send({
+        email: "  ",
+        senha: "senha123",
       });
 
       expect(response.status).toBe(400);
       expect(response.body.errors).toEqual(
         expect.arrayContaining([
-          expect.objectContaining({ msg: 'O email não pode conter apenas espaços em branco' })
+          expect.objectContaining({
+            msg: "O email não pode conter apenas espaços em branco",
+          }),
         ])
       );
     });
 
-    it('deve retornar erro se a senha estiver vazia ou apenas com espaços', async () => {
-      const response = await request(app).post('/login').send({
-        email: 'teste@email.com',
-        senha: '   ',
+    it("deve retornar erro se a senha estiver vazia ou apenas com espaços", async () => {
+      const response = await request(app).post("/login").send({
+        email: "teste@email.com",
+        senha: "   ",
       });
 
       expect(response.status).toBe(400);
       expect(response.body.errors).toEqual(
         expect.arrayContaining([
-          expect.objectContaining({ msg: 'A senha não pode conter apenas espaços em branco' })
+          expect.objectContaining({
+            msg: "A senha não pode conter apenas espaços em branco",
+          }),
         ])
       );
     });
--- Tests/unit/loginController.test.js
+++ Tests/unit/loginController.test.js
@@ -1,13 +1,13 @@
-const { Server } = require('../../src/server');
-const { connection } = require('../../src/database/connection');
-const Usuario = require('../../src/models/Usuario');
-const jwt = require('jsonwebtoken');
-const request = require('supertest');
+const { Server } = require("../../src/server");
+const { connection } = require("../../src/database/connection");
+const Usuario = require("../../src/models/Usuario");
+const jwt = require("jsonwebtoken");
+const request = require("supertest");
 
-describe('Testes de Unidade LoginController', () => {
+describe("Testes de Unidade LoginController", () => {
   let app;
   let usuarioCriado;
-  let senhaUsuario = 'senha123';
+  let senhaUsuario = "senha123";
 
   beforeAll(() => {
     app = new Server().getApp();
@@ -18,8 +18,8 @@ describe('Testes de Unidade LoginController', () => {
     await connection.sync({ force: true });
 
     usuarioCriado = await Usuario.create({
-      nome: 'Usuário Teste',
-      email: 'teste@email.com',
+      nome: "Usuário Teste",
+      email: "teste@email.com",
       senha: senhaUsuario, // A senha será automaticamente hashada pelo beforeSave do modelo
     });
   });
@@ -29,88 +29,85 @@ describe('Testes de Unidade LoginController', () => {
     await connection.close();
   });
 
-  describe('Método logar()', () => {
-    it('deve autenticar um usuário com credenciais válidas e retornar um token', async () => {
-      const response = await request(app)
-        .post('/login')
-        .send({
-          email: 'teste@email.com',
-          senha: senhaUsuario,
-        });
+  describe("Método logar()", () => {
+    it("deve autenticar um usuário com credenciais válidas e retornar um token", async () => {
+      const response = await request(app).post("/login").send({
+        email: "teste@email.com",
+        senha: senhaUsuario,
+      });
 
       expect(response.status).toBe(200);
-      expect(response.body).toHaveProperty('Token');
+      expect(response.body).toHaveProperty("Token");
       // Verificar se o token gerado é um JWT válido
       const decoded = jwt.verify(response.body.Token, process.env.SECRET_JWT);
-      expect(decoded).toHaveProperty('sub', usuarioCriado.id);
+      expect(decoded).toHaveProperty("sub", usuarioCriado.id);
     });
 
-    it('deve retornar erro ao tentar autenticar com senha incorreta', async () => {
-      const response = await request(app)
-        .post('/login')
-        .send({
-          email: 'teste@email.com',
-          senha: 'senhaErrada',
-        });
+    it("deve retornar erro ao tentar autenticar com senha incorreta", async () => {
+      const response = await request(app).post("/login").send({
+        email: "teste@email.com",
+        senha: "senhaErrada",
+      });
 
       expect(response.status).toBe(400);
-      expect(response.body).toHaveProperty('mensagem', 'Senha inválida');
+      expect(response.body).toHaveProperty("mensagem", "Senha inválida");
     });
 
-    it('deve retornar erro ao tentar autenticar com email inexistente', async () => {
-      const response = await request(app)
-        .post('/login')
-        .send({
-          email: 'naoexiste@email.com',
-          senha: 'senha123',
-        });
+    it("deve retornar erro ao tentar autenticar com email inexistente", async () => {
+      const response = await request(app).post("/login").send({
+        email: "naoexiste@email.com",
+        senha: "senha123",
+      });
 
       expect(response.status).toBe(404);
-      expect(response.body).toHaveProperty('erro', 'Email não corresponde a nenhum usuário');
+      expect(response.body).toHaveProperty(
+        "erro",
+        "Email não corresponde a nenhum usuário"
+      );
     });
 
-    it('deve retornar erro se o email estiver vazio ou apenas com espaços', async () => {
-      const response = await request(app)
-        .post('/login')
-        .send({
-          email: '  ',
-          senha: 'senha123',
-        });
+    it("deve retornar erro se o email estiver vazio ou apenas com espaços", async () => {
+      const response = await request(app).post("/login").send({
+        email: "  ",
+        senha: "senha123",
+      });
 
       expect(response.status).toBe(400);
       expect(response.body.errors).toEqual(
         expect.arrayContaining([
-          expect.objectContaining({ msg: 'O email não pode conter apenas espaços em branco' })
+          expect.objectContaining({
+            msg: "O email não pode conter apenas espaços em branco",
+          }),
         ])
       );
     });
 
-    it('deve retornar erro se a senha estiver vazia ou apenas com espaços', async () => {
-      const response = await request(app)
-        .post('/login')
-        .send({
-          email: 'teste@email.com',
-          senha: '   ',
-        });
+    it("deve retornar erro se a senha estiver vazia ou apenas com espaços", async () => {
+      const response = await request(app).post("/login").send({
+        email: "teste@email.com",
+        senha: "   ",
+      });
 
       expect(response.status).toBe(400);
       expect(response.body.errors).toEqual(
         expect.arrayContaining([
-          expect.objectContaining({ msg: 'A senha não pode conter apenas espaços em branco' })
+          expect.objectContaining({
+            msg: "A senha não pode conter apenas espaços em branco",
+          }),
         ])
       );
     });
 
-    it('deve retornar erro se os dados de login não forem enviados', async () => {
-      const response = await request(app)
-        .post('/login')
-        .send({});
+    it("deve retornar erro se os dados de login não forem enviados", async () => {
+      const response = await request(app).post("/login").send({});
 
       expect(response.status).toBe(400);
       expect(response.body.errors).toEqual(
         expect.arrayContaining([
-          expect.objectContaining({ msg: 'Email inválido' }),
-          expect.objectContaining({ msg: 'A senha deve ter pelo menos 4 caracteres' }),
+          expect.objectContaining({ msg: "Email inválido" }),
+          expect.objectContaining({
+            msg: "A senha deve ter pelo menos 4 caracteres",
+          }),
         ])
       );
     });
